3ef2d070ffba43230c60f0c0d741c8c1bbcd1f76
Isaac Sears <isaac.j.sears@gmail.com>
2022-07-04 08:03:21 -0400

    Update dataprocessing slurmjob

--- slurm/mimic2ts.sh
+++ slurm/mimic2ts.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 #SBATCH -n 1
 #SBATCH -p batch
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=32G
-#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=64G
+#SBATCH --time=12:00:00
 #SBATCH --output ./logs/mimic2ts-%j.log
 
-export PYTHONUNBUFFERED=TRUE
-export PYTHONPATH=./:$PYTHONPATH
 
 cd ~/Repos/tabnet-sepsis/
 
-python -m mimic2ts --src mimiciv --dst mimicts
+python3 -m mimic2ts --timestep 21600 --blocksize 25000000 mimiciv mimicts 
