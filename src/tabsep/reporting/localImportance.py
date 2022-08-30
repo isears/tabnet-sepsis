5707e1ea8dbccd1ec80e9ffece821004de9f366a
Isaac Sears <isaac.j.sears@gmail.com>
2022-08-30 17:44:37 -0400

    Generate case studies and add feature value annotations

--- src/tabsep/reporting/localImportance.py
+++ src/tabsep/reporting/localImportance.py
@@ -18,29 +18,50 @@ if __name__ == "__main__":
 
     sample_idx = np.argmax(
         torch.logical_and(
-            torch.logical_and(y_test == 1, torch.sum(X_test[:, :, -1], dim=1) > 3,),
-            torch.logical_and(torch.sum(X_test[:, :, -1], dim=1) < 40, preds > 0.8,),
+            torch.logical_and(y_test == 1, torch.sum(X_test[:, :, -1], dim=1) > 7,),
+            torch.logical_and(torch.sum(X_test[:, :, -1], dim=1) < 30, preds > 0.6),
         )
     )
 
     print(f"Analyzing local importance of idx {sample_idx}")
-    sample_case = pd.DataFrame(attributions[sample_idx].cpu().detach().numpy())
+    sample_case_attrs = pd.DataFrame(attributions[sample_idx].cpu().detach().numpy())
+    sample_case_values = pd.DataFrame(X_test[sample_idx, :, :-1].cpu().detach().numpy())
 
-    sample_case.columns = get_feature_labels()
+    sample_case_attrs.columns = get_feature_labels()
+    sample_case_values.columns = get_feature_labels()
+
     # Truncate by padding mask
-    sample_case = sample_case[pad_masks[sample_idx].tolist()]
-    max_absolute_attribution = sample_case.abs().apply(lambda col: col.sum())
+    sample_case_attrs = sample_case_attrs[pad_masks[sample_idx].tolist()]
+    sample_case_values = sample_case_values[pad_masks[sample_idx].tolist()]
+
+    max_absolute_attribution = sample_case_attrs.abs().apply(lambda col: col.sum())
     top_n_features = max_absolute_attribution.nlargest(n=10).index
 
-    sample_case = sample_case.drop(
-        columns=[c for c in sample_case.columns if c not in top_n_features]
+    sample_case_attrs = sample_case_attrs.drop(
+        columns=[c for c in sample_case_attrs.columns if c not in top_n_features]
     )
 
-    sample_case.index = sample_case.index * (config.timestep_seconds / (60 * 60))
+    sample_case_values = sample_case_values.drop(
+        columns=[c for c in sample_case_values.columns if c not in top_n_features]
+    )
 
-    sample_case.index.name = "Time in ICU (hrs.)"
+    sample_case_attrs.index = sample_case_attrs.index * (
+        config.timestep_seconds / (60 * 60)
+    )
+    sample_case_values.index = sample_case_values.index * (
+        config.timestep_seconds / (60 * 60)
+    )
 
-    ax = sns.heatmap(sample_case.transpose(), linewidths=0.01, linecolor="black")
+    sample_case_attrs.index.name = "Time in ICU (hrs.)"
+
+    fig, ax = plt.subplots(figsize=(20, 20))
+    sns.heatmap(
+        sample_case_attrs.transpose(),
+        linewidths=0.01,
+        linecolor="black",
+        annot=sample_case_values.transpose(),
+        ax=ax,
+    )
     ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right")
     ax.set_yticklabels(ax.get_yticklabels(), rotation=0)
     ax.set_title(
