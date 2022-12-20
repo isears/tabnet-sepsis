4a54f9f7c8326acf05b0c9f763b00a662dc0532e
isears <isaac.j.sears@gmail.com>
2022-12-20 10:22:10 -0500

    Try to scale the data for the TST like it's scaled for LR

--- TODO.md
+++ TODO.md
@@ -1,8 +1,11 @@
 # TODOs for Coagulopathy Algorithm
-[] Generate global feature importances
+[] Scale the inputs of the TST
+[x] Generate global feature importances
 [] Generate local feature importance
 [x] Modify inclusion criteria
 [x] Modify postprocessor
+[] More options for tuning of TST based on other layer types available in the original github repo
 [] Modify TST to do regression instead of classification
 [] Measure RMSE instead of AUC
 [] Verify that models do not have access to the measurement they are trying to predict
+[] Run more intense integrated gradients method on CCV
--- src/tabsep/modeling/singleTst.py
+++ src/tabsep/modeling/singleTst.py
@@ -2,7 +2,10 @@
 Run TST w/specific hyperparameters
 """
 
+import numpy as np
 from sklearn.metrics import average_precision_score, roc_auc_score
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 
 from tabsep.dataProcessing.fileBasedDataset import FileBasedDataset
 from tabsep.dataProcessing.labelGeneratingDataset import CoagulopathyDataset
@@ -18,6 +21,46 @@ PARAMS = dict(
     iterator_train__batch_size=16,  # Should be 128
 )
 
+
+class TimeSeriesScaler(StandardScaler):
+    """
+    Scale the features one at a time
+    Assumes shape of data is (n_samples, seq_len, n_features)
+    """
+
+    def __init__(self, *, copy=True, with_mean=True, with_std=True):
+        self.copy = copy
+        self.with_mean = with_mean
+        self.with_std = with_std
+
+    def fit(self, X, y=None, sample_weight=None):
+        self.feature_scalers = list()
+
+        for feature_idx in range(0, X.shape[-1]):  # Assuming feature_dim is last dim
+            feature_scaler = StandardScaler(
+                copy=self.copy, with_mean=self.with_mean, with_std=self.with_std
+            )
+            feature_scaler.fit(X[:, :, feature_idx])
+            self.feature_scalers.append(feature_scaler)
+
+        return self
+
+    def transform(self, X, copy=None):
+        return np.stack(
+            [f.transform(X[:, :, idx]) for idx, f in enumerate(self.feature_scalers)],
+            axis=-1,
+        )
+
+    def inverse_transform(self, X, copy=None):
+        return np.stack(
+            [
+                f.reverse_tranform(X[:, :, idx])
+                for idx, f in enumerate(self.feature_scalers)
+            ],
+            axis=1,
+        )
+
+
 if __name__ == "__main__":
     train_sids, test_sids = split_data_consistently()
 
@@ -25,8 +68,9 @@ if __name__ == "__main__":
     test_ds = CoagulopathyDataset(test_sids)
 
     tst = tunable_tst_factory(PARAMS, train_ds, save_path="cache/models/singleTst")
+    scaled_tst = make_pipeline([TimeSeriesScaler(), tst])
 
-    tst.fit(train_ds, y=None)
+    scaled_tst.fit(train_ds, y=None)
 
     # TODO: dont' have get_labels() anymore need to update this
     # final_auroc = roc_auc_score(test_ds.get_labels(), tst.predict_proba(test_ds)[:, 1])
