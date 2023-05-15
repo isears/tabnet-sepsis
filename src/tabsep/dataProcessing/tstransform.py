e1b51ac4c1dbf2ba4758e04cd3480ef7ab0ffb46
isears <isaac.j.sears@gmail.com>
2023-05-15 13:13:29 -0400

    Add sepsis

--- src/tabsep/dataProcessing/tstransform.py
+++ src/tabsep/dataProcessing/tstransform.py
@@ -114,12 +114,6 @@ class Derived2Ts:
         )
 
         stay_group = stay_group.reindex(daterange)
-
-        # TODO: planning to leave this to the dataset so that normalization can be applied before imputation
-        # Forward fill where possible, otherwise fill w/0.0
-        # stay_group = stay_group.fillna(method="ffill")
-        # stay_group = stay_group.fillna(0.0)
-
         stay_group.to_parquet(f"{self.save_path}/{stay_id}.vitalsign.parquet")
 
     def agg_bg_group(self, stay_group):
@@ -164,6 +158,37 @@ class Derived2Ts:
         stay_group = stay_group.reindex(daterange)
         stay_group.to_parquet(f"{self.save_path}/{stay_id}.bg.parquet")
 
+    def agg_sepsis_group(self, stay_group):
+        stay_id = stay_group.name
+        daterange = self._generate_daterange(stay_id)
+
+        # Decrease charttime resolution to hourly
+        stay_group = self._convert_to_hourly_resolution(stay_group, "sofa_time")
+        stay_group = self._convert_to_hourly_resolution(
+            stay_group, "suspected_infection_time"
+        )
+
+        stay_group["sepsis_time"] = stay_group.apply(
+            lambda x: max(x["sofa_time"], x["suspected_infection_time"]), axis=1
+        )
+
+        stay_group["sepsis3"] = stay_group["sepsis3"].astype(float)
+
+        stay_group = (
+            stay_group[
+                [
+                    "sepsis_time",
+                    "sepsis3",
+                ]
+            ]
+            .groupby("sepsis_time")
+            .agg("max")
+        )
+
+        stay_group = stay_group.reindex(daterange)
+        stay_group = stay_group.fillna(0.0)
+        stay_group.to_parquet(f"{self.save_path}/{stay_id}.sepsis3.parquet")
+
     def agg_default_measurement_group(self, stay_group, name):
         """
         Most groups will require similar processing
@@ -184,11 +209,15 @@ class Derived2Ts:
         stay_group.to_parquet(f"{self.save_path}/{stay_id}.{name}.parquet")
 
     def run_all(self):
-        # print("[*] Running vitals...")
+        print("[*] Aggregating sepsis3...")
+        sepsis3 = pd.read_parquet("mimiciv_derived/sepsis3.parquet")
+        sepsis3.groupby("stay_id").apply(self.agg_sepsis_group)
+
+        # print("[*] Aggregating vitals...")
         # vitals = pd.read_parquet("mimiciv_derived/vitalsign.parquet")
         # vitals.groupby("stay_id").apply(self.agg_vitals_group)
 
-        # print("[*] Running bg...")
+        # print("[*] Aggregating bg...")
         # bg = self._load_clean("mimiciv_derived/bg.parquet")
         # bg.groupby("stay_id").apply(self.agg_bg_group)
 
@@ -208,6 +237,18 @@ class Derived2Ts:
             df.groupby("stay_id").apply(
                 lambda g: self.agg_default_measurement_group(g, table_name)
             )
+
+        meds_tables = [
+            "dobutamine",
+            "dopamine",
+            "epinephrine",
+            "invasive_line",
+            "milrinone",
+            "norepinephrine",
+            "phenylephrine",
+            "vasopressin",
+            "ventilation",
+        ]
 
 
 if __name__ == "__main__":
