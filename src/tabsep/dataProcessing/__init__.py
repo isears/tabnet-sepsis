938349c8f77998dc49b27683dc1d01ad5cd023d8
isears <isaac.j.sears@gmail.com>
2023-06-26 16:14:21 -0400

    Add option to get normalized values

--- src/tabsep/dataProcessing/__init__.py
+++ src/tabsep/dataProcessing/__init__.py
@@ -17,3 +17,24 @@ class LabeledSparseTensor:
     features: list
     X_sparse: torch.Tensor
     y: torch.Tensor
+
+    def get_dense_normalized(self):
+        X_dense = self.X_sparse.to_dense()
+
+        n = torch.count_nonzero(X_dense, dim=(0, -1))
+        s = torch.sum(X_dense, dim=(0, -1))
+        mu = s / n
+
+        mu = mu.unsqueeze(0).unsqueeze(-1)
+        n = n.unsqueeze(0).unsqueeze(-1)
+
+        mu_diff_2 = torch.where(
+            X_dense != 0.0,
+            (X_dense - mu) ** 2,
+            0.0,
+        )
+        std = torch.sqrt(
+            torch.sum(mu_diff_2, dim=(0, -1)).unsqueeze(0).unsqueeze(-1) / (n - 1)
+        )
+
+        return torch.where(X_dense != 0.0, (X_dense - mu) / std, 0.0)
