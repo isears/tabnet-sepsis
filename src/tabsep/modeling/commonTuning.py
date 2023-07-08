4b8f1b0d08dd6fa41d8b23dbbc490a78883178d1
isears <isaac.j.sears@gmail.com>
2023-07-08 08:10:03 -0400

    Remove dead code

--- src/tabsep/modeling/commonTuning.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-from tabsep.modeling import CVResults, TabsepModelFactory
-from tabsep.modeling.commonCV import cv_runner
-
-
-def tuning_runner(model_factory: TabsepModelFactory):
-    pass
