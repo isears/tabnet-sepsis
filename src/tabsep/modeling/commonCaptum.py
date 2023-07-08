586538da092e54b9266e5de842e98933791137d3
isears <isaac.j.sears@gmail.com>
2023-07-08 13:46:01 -0400

    Batch captum attribs

--- src/tabsep/modeling/commonCaptum.py
+++ src/tabsep/modeling/commonCaptum.py
@@ -12,6 +12,7 @@ from captum.attr import (
     Saliency,
     ShapleyValueSampling,
 )
+from tqdm import tqdm
 
 torch.manual_seed(42)
 np.random.seed(42)
@@ -22,9 +23,9 @@ os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
 CORES_AVAILABLE = len(os.sched_getaffinity(0))
 
 
-def captum_runner(trained_model: torch.nn.Module, X, batch_size=16):
-    # X = X.to("cuda")
-    # trained_model = trained_model.to("cuda")
+def captum_runner(trained_model: torch.nn.Module, X, batch_size=2):
+    X = X.to("cuda")
+    trained_model = trained_model.to("cuda")
 
     model_name = trained_model.__class__.__name__
     trained_model.eval()
@@ -32,7 +33,15 @@ def captum_runner(trained_model: torch.nn.Module, X, batch_size=16):
     X.requires_grad = True
 
     attributor = IntegratedGradients(trained_model)
-    attributions = attributor.attribute(X, target=0)
+    attributions = list()
+
+    for batch_idx in tqdm(range(batch_size, X.shape[0], batch_size)):
+        end_idx = min(batch_size + batch_idx, X.shape[0])
+        attributions.append(attributor.attribute(X[batch_idx:end_idx], target=0))
+
+    attributions = torch.concat(attributions, dim=0)
+
+    assert attributions.shape == X.shape
 
     os.makedirs(f"cache/captum/{model_name}", exist_ok=True)
     torch.save(attributions, f"cache/captum/{model_name}/attributions.pt")
